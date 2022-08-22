1b6c4a5fa369a47b41dec02bf103d9105d7228e1
Saidur Rahman <saidurrahman1473@gmail.com>
2022-08-22 15:01:18 +0600

    finish design

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
-document.getElementById('calculate').addEventListener('click',function(){
+// document.getElementById('calculate').addEventListener('click',function(){
    
-    const perPlayerField = getInputFieldValueById('perPlayer-field');
+//     const perPlayerField = getInputFieldValueById('perPlayer-field');
     
-    const playerExpensesTotal = perPlayerField * 5;
+//     const playerExpensesTotal = perPlayerField * 5;
 
-    const playerExpensesShow =  getElementValueById('player-expenses')
+//     const playerExpensesShow =  getElementValueById('player-expenses')
 
-    setTextElementValueById('player-expenses', playerExpensesTotal)
-})
-
-
+//     setTextElementValueById('player-expenses', playerExpensesTotal)
+// })
 
 
 
 
-document.getElementById('calculate-total').addEventListener('click',function(){
+
+
+// document.getElementById('calculate-total').addEventListener('click',function(){
    
-    const playerExpensesShow =  getElementValueById('player-expenses')
+//     const playerExpensesShow =  getElementValueById('player-expenses')
 
-    const managerField = getInputFieldValueById('manager-field');
-    const coachField = getInputFieldValueById('coach-field');
+//     const managerField = getInputFieldValueById('manager-field');
+//     const coachField = getInputFieldValueById('coach-field');
 
-    const totalCost = playerExpensesShow + managerField + coachField;
+//     const totalCost = playerExpensesShow + managerField + coachField;
 
-    setTextElementValueById('final-total',totalCost)
-})
-
-
+//     setTextElementValueById('final-total',totalCost)
+// })
 
 
-document.getElementById('btn-select').addEventListener('click', function(){
 
-   const playerShow = document.getElementById('player-show')
+
+// document.getElementById('btn-select').addEventListener('click', function(){
+
+//    const playerShow = document.getElementById('player-show')
   
-})
+// })
