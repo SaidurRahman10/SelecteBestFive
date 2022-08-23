e361856b57e227e644d7d1d79ee358d72363a16e
Saidur Rahman <saidurrahman1473@gmail.com>
2022-08-23 10:13:40 +0600

    last problem

--- js/app.js
+++ js/app.js
@@ -1,36 +1,85 @@
-document.getElementById('calculate').addEventListener('click',function(){
-   
-    const perPlayerField = getInputFieldValueById('perPlayer-field');
-    
+document.getElementById("calculate").addEventListener("click", function () {
+  const perPlayerField = getInputFieldValueById("perPlayer-field");
+
+  if (isNaN(perPlayerField)) {
+    alert("Error!!! Please Enter a Valid Number");
+  } else {
     const playerExpensesTotal = perPlayerField * 5;
 
-    const playerExpensesShow =  getElementValueById('player-expenses')
+    const playerExpensesShow = getElementValueById("player-expenses");
 
-    setTextElementValueById('player-expenses', playerExpensesTotal)
-})
+    setTextElementValueById("player-expenses", playerExpensesTotal);
+  }
+});
 
+document
+  .getElementById("calculate-total")
+  .addEventListener("click", function () {
+    
+    const managerField = getInputFieldValueById("manager-field");
+    const coachField = getInputFieldValueById("coach-field");
+    if(isNaN(managerField)){
+      alert("Error!!! Please Enter a Valid Number");
+    }else if(isNaN(coachField)){
+      alert("Error!!! Please Enter a Valid Number");
 
-
-
-
-
-document.getElementById('calculate-total').addEventListener('click',function(){
-   
-    const playerExpensesShow =  getElementValueById('player-expenses')
-
-    const managerField = getInputFieldValueById('manager-field');
-    const coachField = getInputFieldValueById('coach-field');
-
-    const totalCost = playerExpensesShow + managerField + coachField;
-
-    setTextElementValueById('final-total',totalCost)
-})
-
-
-
-
-document.getElementById('btn-select').addEventListener('click', function(){
-
-   const playerShow = document.getElementById('player-show')
+    }else{
+      
+      const playerExpensesShow = getElementValueById("player-expenses");
+      const totalCost = playerExpensesShow + managerField + coachField;
   
-})
+      setTextElementValueById("final-total", totalCost);
+    }
+  });
+
+//selected Buttons
+
+//taskin
+document.getElementById("btn-taskin").addEventListener("click", function () {
+  const playerShow = document.getElementById("player-show");
+  const li = document.createElement("li");
+  li.innerText = "    Taskin Ahammed";
+
+  playerShow.appendChild(li);
+});
+//shakib
+document.getElementById("btn-shakib").addEventListener("click", function () {
+  const playerShow = document.getElementById("player-show");
+  const li = document.createElement("li");
+  li.innerText = "    Shakib Al Hasan";
+  playerShow.appendChild(li);
+});
+//mashrafe
+document.getElementById("btn-mashrafe").addEventListener("click", function () {
+  const playerShow = document.getElementById("player-show");
+  const li = document.createElement("li");
+  li.innerText = "    Mashrafe bin Mortaza";
+  playerShow.appendChild(li);
+});
+// Mushfiqur
+document.getElementById("btn-mushfiqur").addEventListener("click", function () {
+  const playerShow = document.getElementById("player-show");
+  const li = document.createElement("li");
+  li.innerText = " Mushfiqur Rahim";
+  playerShow.appendChild(li);
+});
+// Mustafizur
+document.getElementById("btn-mustafiz").addEventListener("click", function () {
+  const playerShow = document.getElementById("player-show");
+  const li = document.createElement("li");
+  li.innerText = "Mustafizur Rahman";
+  playerShow.appendChild(li);
+});
+//Mahmudullah
+document
+  .getElementById("btn-mahmudullah")
+  .addEventListener("click", function () {
+    const playerShow = document.getElementById("player-show");
+    const li = document.createElement("li");
+    li.innerText = " Mahmudullah Riyad";
+    playerShow.appendChild(li);
+
+    if (li.length == 5) {
+      alert("U cant add more than 5");
+    }
+  });
--- js/common.js
+++ js/common.js
@@ -20,3 +20,49 @@ function setTextElementValueById(id, value) {
   const element = document.getElementById(id);
   element.innerText = value;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//button disable
+// <!-- working this disable button -->
+function DisableButton() {
+  document.getElementById("btn-taskin").disabled = "true";
+}
+
+function taskin() {
+  document.getElementById("btn-taskin").disabled = true;
+}
+function shakib() {
+  document.getElementById("btn-shakib").disabled = true;
+}
+function mashrafe() {
+  document.getElementById("btn-mashrafe").disabled = true;
+}
+function mushfiqur() {
+  document.getElementById("btn-mushfiqur").disabled = true;
+}
+function mustafiz() {
+  document.getElementById("btn-mustafiz").disabled = true;
+}
+function mahmudullah() {
+  document.getElementById("btn-mahmudullah").disabled = true;
+}
